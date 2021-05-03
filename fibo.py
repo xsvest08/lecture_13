d8cb22e7202acdfc816098807549edd45fe22b46
xsvest08 <emikbvb@seznam.cz>
2021-05-03 10:39:21 +0200

    hotovo

--- fibo.py
+++ fibo.py
@@ -1,6 +1,17 @@
+def recursive_nht_fibo(n):
+    if n== 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return (recursive_nht_fibo(n-1) + recursive_nht_fibo(n-2))
+
+
 
 def main():
-    pass
+    number = int(input("kolik prvků chceš"))
+    fs = [recursive_nht_fibo(n) for i in range(number)]
+    print(fs)
 
 
 if __name__ == '__main__':
